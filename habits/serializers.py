0a9cfdb894e089f2b2c83630818bbefa4afa9b77
Aleksey-Markov <tte4enuxa@gmail.com>
2024-09-08 17:28:17 +0700

    добавил пагинацию и кастомные права доступа

---
+++ habits/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from habits.models import Habit
+
+
+class HabitSerializer(serializers.Serializer):
+    class Meta:
+        model = Habit
+        fields = "__all__"
--- habits/urls.py
+++ habits/urls.py
@@ -1,5 +1,13 @@
 from habits.apps import HabitsConfig
+from django.urls import path
+
 
 app_name = HabitsConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('list/'),
+    path('public/'),
+    path('create/'),
+    path('update/<int:pk>/'),
+    path('delete/<int:pk>/'),
+]
