807da00c35ab5afaae0bb7c94b3a1ffd66a6c5f3
Aleksey-Markov <tte4enuxa@gmail.com>
2024-09-08 18:02:23 +0700

    настроил эндпоинты для привычек

--- habits/urls.py
+++ habits/urls.py
@@ -1,13 +1,16 @@
 from habits.apps import HabitsConfig
 from django.urls import path
+from habits.views import (HabitCreateView, HabitListView, HabitPublicListView, HabitRetrieveView, HabitUpdateView,
+                          HabitDeleteView)
 
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path('list/'),
-    path('public/'),
-    path('create/'),
-    path('update/<int:pk>/'),
-    path('delete/<int:pk>/'),
+    path('list/', HabitListView.as_view(), name='list'),
+    path('public/', HabitPublicListView.as_view(), name='public'),
+    path('<int:pk>/', HabitRetrieveView.as_view(), name='detail'),
+    path('create/', HabitCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', HabitUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', HabitDeleteView.as_view(), name='delete'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,36 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from habits.models import Habit
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
+class HabitCreateView(CreateAPIView):
+    serializer_class = HabitSerializer
 
+
+class HabitListView(ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
+
+
+class HabitPublicListView(ListAPIView):
+    queryset = Habit.objects.filter(is_public=True)
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveView(RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
+
+
+class HabitUpdateView(UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
+
+
+class HabitDeleteView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
